4bf6ca1ebb25f0721fcd92641e8bebb103522f39
Alexey Kremnev <alexeykremnev@localhost.localdomain>
2020-07-04 22:43:08 -0600

    first commit

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/src/Graphics.cpp" "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/src/Intersection.cpp" "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/src/Street.cpp" "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/src/TrafficObject.cpp" "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/src/TrafficSimulator-L1.cpp" "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-L1.cpp.o"
+  "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/src/Vehicle_Student.cpp" "/home/alexeykremnev/Documents/cpp_projects/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle_Student.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
